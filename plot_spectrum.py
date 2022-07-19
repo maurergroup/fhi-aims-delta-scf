1cf67975b5b037506b9c25e78f0c669ace6a7bbf
Dylan Morgan <dylan.morgan@warwick.ac.uk>
2022-07-19 12:02:09 +0100

    Added option to change atom for plotting spectrum

--- plot_spectrum.py
+++ plot_spectrum.py
@@ -3,11 +3,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x_axis = np.loadtxt('./C_xps_spectrum.txt', usecols=(0))
-y_axis = np.loadtxt('./C_xps_spectrum.txt', usecols=(1))
+atom = str(input('Enter atom: '))
+
+x_axis = np.loadtxt(f'./{atom}_xps_spectrum.txt', usecols=(0))
+y_axis = np.loadtxt(f'./{atom}_xps_spectrum.txt', usecols=(1))
 
 plt.xlabel('Energy / eV')
 plt.ylabel('Intensity')
 
 plt.plot(x_axis, y_axis)
-plt.savefig('spectrum.png')
+plt.savefig(f'{atom}_spectrum.png')
+
+print(f'Spectrum saved as {atom}_spectrum.png')
