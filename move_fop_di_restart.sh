bb87a2e428b2dc987d6b051532726fe4cc0c974b
Dylan <Dylan.Morgan@warwick.ac.uk>
2022-07-20 22:36:50 +0100

    move restart files script for double init method

--- move_fop_di_restart.sh
+++ move_fop_di_restart.sh
@@ -3,10 +3,14 @@
 read -p 'Enter atom: ' atom
 
 for dir in "$atom"*; do
-  for file in "$dir/init_1/restart"*; do
-    mv "$file" "$dir/init_2/"
-  done
-  for file in "$dir/init_2/restart"*; do
-    mv "$file" "$dir/hole/"
-  done
+  if [[ "$1" == 1 ]]; then
+    for file in "$dir/init_1/restart"*; do
+      mv "$file" "$dir/init_2/"
+    done
+
+  elif [[ "$1" == 2 ]]; then
+    for file in "$dir/init_2/restart"*; do
+      mv "$file" "$dir/hole/"
+    done
+  fi
 done
