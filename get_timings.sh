7fb9973ba894bead1a7aa0b0a3db5e946d86cc4f
Dylan Morgan <dylan.morgan@warwick.ac.uk>
2022-08-08 16:46:47 +0100

    Added finding average scf timings functionality

--- get_timings.sh
+++ get_timings.sh
@@ -1,68 +1,132 @@
 #!/bin/bash
 
-if [[ $1 == '' ]]; then
+script_usage () {
+  echo "Script Usage:"
+  echo "1st argument: Time to parse from aims.out (options: scf or total)"
+  echo "2nd argument: Directories to search for timings"
+  exit 1
+}
+
+# Argument checker
+if [[ $1 != "total" && $1 != "scf" ]]; then
+  echo "Incorrect timing type argument specified"
+  echo
+  script_usage
+elif [[ $2 == "" ]]; then
   echo "Directories argument not specified"
   echo
-  echo "Script Usage:"
-  echo "1st argument: Directories to search for timings"
-  exit 1
+  script_usage
 fi
 
-for i in "$@"; do
+scf_timings () {
+  # Read scf timings from aims.out and add to array
+  scf_times+=($(cat aims.out | grep '| Time for this iteration' | awk '{ print $7 }'))
+
+  # Initisalise counters and penultimate index of scf_times
+  iter_count_1=0
+  iter_count_2=0
+  scf_times_len_short=$(("${#scf_times[@]}" - 1))
+
+  # Difference between kth value and k+1 value
+  for k in "${scf_times[@]::$scf_times_len_short}"; do
+    iter_count_1=$((iter_count_1 + 1))
+    current_iter=$(echo "${scf_times[$iter_count_2]}")
+    next_iter=$(echo "${scf_times[$iter_count_1]}")
+    iter_diff=$(echo "$current_iter - $next_iter" | bc -l)
+
+    # Use values after first value where difference between scf values is
+    # less than 1
+    if (( $(echo "$iter_diff < 1" | bc -l) )); then
+      stab_times_index="$iter_count_2"
+      break
+    fi
+
+    iter_count_2=$((iter_count_2 + 1))
+  done
+
+  # New array of stablised scf times
+  stab_scf_times+=("${scf_times[@]:$stab_times_index}")
+
+  # Sum all scf times
+  stab_scf_times_tot=0
+  for k in "${stab_scf_times[@]}"; do
+    stab_scf_times_tot=$(echo "$stab_scf_times_tot + $k" | bc -l)
+  done
+
+  # Average and round time per scf step
+  avg_stab_scf_time=$(echo "$stab_scf_times_tot" / "${#stab_scf_times[@]}" | bc -l)
+  round_stab_scf_time=$(printf "%.2f\n" "$avg_stab_scf_time")
+
+  echo "$i $j"
+  # echo "${scf_times[@]:$stablised_times_index}"
+  echo "Average time per SCF step: $round_stab_scf_time sec"
+  echo
+}
+
+# Add all user specified directories to an array
+for i in "${@:2}"; do
   input_dirs+=("$i")
 done
 
 echo "Specified directories:" "${input_dirs[@]}"
 echo
 
-hole_times_sec=()
-hole_times_min=()
-init1_times_sec=()
-init1_times_min=()
-init2_times_sec=()
-init2_times_min=()
-
+# Go into directories and files to read timings
 for i in "${input_dirs[@]}"; do
-  atom_counter+=1
   cd "$i"
-  for j in *; do
-    if [ -d "$j" ]; then
-      # If a directory, this is an excited state calculation
-      atom="$i"
-      run_type="$j"
-      cd "$j"
-      # Find time taken in aims.out
-      time=$(tail -n 100 aims.out | grep '| Total time   ' | awk '{ print $5 }')
-      time_round=$(printf "%.0f\n" "$time")
-      time_h=$(echo "$time_round" / 60 | bc -l)
-      time_hr=$(printf "%.2f\n" "$time_h")
-      echo "$atom $run_type: $time_round seconds"
-      echo "$atom $run_type: $time_hr minutes"
-      echo
 
-      # Add time to array depending on run type
-      if [[ "$run_type" == "hole" ]]; then
-        hole_times_sec+=("$time_round")
-        hole_times_min+=("$time_hr")
-      elif [[ "$run_type" == "init_1" ]]; then
-        init1_times_sec+=("$time_round")
-        init1_times_min+=("$time_hr")
-      elif [[ "$run_type" == "init_2" ]]; then
-        init2_times_sec+=("$time_round")
-        init2_times_min+=("$time_hr")
+  for j in *; do
+    run_type="$j"
+    if [[ $1 == "scf" ]]; then
+      scf_times=()
+    fi
+
+    if [ -d "$j" ]; then
+      if [[ $1 == "total" ]]; then
+        # If a directory, this is an excited state calculation
+        atom="$i"
+        cd "$j"
+        # Find time taken in aims.out
+        time=$(tail -n 100 aims.out | grep '| Total time   ' | awk '{ print $5 }')
+        time_round=$(printf "%.0f\n" "$time")
+        time_h=$(echo "$time_round" / 60 | bc -l)
+        time_hr=$(printf "%.2f\n" "$time_h")
+        echo "$atom $run_type: $time_round seconds"
+        echo "$atom $run_type: $time_hr minutes"
+        echo
+
+        # Add time to array depending on run type
+        if [[ "$run_type" == "hole" ]]; then
+          hole_times_sec+=("$time_round")
+          hole_times_min+=("$time_hr")
+        elif [[ "$run_type" == "init_1" ]]; then
+          init1_times_sec+=("$time_round")
+          init1_times_min+=("$time_hr")
+        elif [[ "$run_type" == "init_2" ]]; then
+          init2_times_sec+=("$time_round")
+          init2_times_min+=("$time_hr")
+        fi
+
+      elif [[ $1 == "scf" ]]; then
+        cd "$j"
+        scf_timings
       fi
 
     elif [[ "$j" == "aims.out" ]]; then
-      # This must be a ground state calculation
-      # Find time taken in aims.out
-      ground_state="true"
-      time=$(tail -n 100 "$j" | grep '| Total time   ' | awk '{ print $5 }')
-      time_round=$(printf "%.0f\n" "$time")
-      time_h=$(echo "$time_round" / 60 | bc -l)
-      time_hr=$(printf "%.2f\n" "$time_h")
-      echo "ground: $time_round seconds"
-      echo "ground: $time_hr minutes"
-      echo
+      if [[ "$1" == "total" ]]; then
+        # This must be a ground state calculation
+        # Find time taken in aims.out
+        ground_state="true"
+        time=$(tail -n 100 "$j" | grep '| Total time   ' | awk '{ print $5 }')
+        time_round=$(printf "%.0f\n" "$time")
+        time_h=$(echo "$time_round" / 60 | bc -l)
+        time_hr=$(printf "%.2f\n" "$time_h")
+        echo "ground: $time_round seconds"
+        echo "ground: $time_hr minutes"
+        echo
+      elif [[ "$1" == "scf" ]]; then
+        scf_timings
+      fi
     fi
 
     cd ../
@@ -71,7 +135,7 @@ for i in "${input_dirs[@]}"; do
   cd ../
 done
 
-if [[ "$ground_state" != "true" ]]; then
+if [[ "$1" == "total" && "$ground_state" != "true" ]]; then
   # Sum counters
   hole_sec_tot=0
   init1_sec_tot=0
@@ -108,3 +172,4 @@ if [[ "$ground_state" != "true" ]]; then
   echo "Average init_2 time: $round_init2_time_sec seconds"
   echo "Average init_2 time: $round_init2_time_min minutes"
 fi
+
